8019e0928d6e6fe5b71e14b88d57ce7284b31a7c
Mika Tammi <mikatammi@gmail.com>
2023-10-01 15:24:42 +0300

    Preliminary network configuration
    Signed-off-by: Mika Tammi <mikatammi@gmail.com>

--- nuc-router/default.nix
+++ nuc-router/default.nix
@@ -58,6 +58,84 @@
     ];
 
     system.stateVersion = "23.05";
+
+    # Networking configuration
+
+    networking.enableIPv6 = false;
+
+    # Set static interface names for ethernet devices used in configuration
+    systemd.network.links."10-ethwan0" = {
+      matchConfig.PermanentMACAddress = externalMac;
+      linkConfig.Name = "ethwan0";
+    };
+    systemd.network.links."10-ethwlan0" = {
+      matchConfig.PermanentMACAddress = wlanMac;
+      linkConfig.Name = "wlan0";
+    };
+    systemd.network.links."10-ethlan0" = {
+      matchConfig.PermanentMACAddress = internalMac;
+      linkConfig.Name = "ethlan0";
+    };
+
+    networking.firewall.enable = true;
+    networking.firewall.allowedTCPPorts = [
+      22 # SSH
+    ];
+    networking.firewall.interfaces."ethlan0".allowedUDPPorts = [
+      67 # DHCP Server
+    ];
+
+    # Use DHCP to get external IP address
+    networking.interfaces.ethwan0.useDHCP = true;
+
+    # Internal IP address
+    networking.interfaces.ethlan0.ipv4.addresses = [
+      {
+        address = "10.42.0.1";
+        prefixLength = 16; # subnet mask 255.255.0.0
+      }
+    ];
+    networking.nat = {
+      enable = true;
+      internalInterfaces = ["ethlan0"];
+      externalInterface = "ethwan0";
+      forwardPorts = [
+        # Example port forwards:
+        # {
+        #   sourcePort = 12000;
+        #   destination = "10.0.0.2:22";
+        #   proto = "tcp";
+        # }
+        # {
+        #   sourcePort = 12001;
+        #   destination = "10.0.0.12:12001";
+        #   proto = "udp";
+        # }
+      ];
+    };
+    services.dhcpd4 = {
+      enable = true;
+      interfaces = ["ethlan0"]; # Only serve DHCP addresses towards LAN
+      authoritative = true;
+      machines = [
+        {
+          hostName = "bf";
+          ipAddress = "10.42.0.42";
+          ethernetAddress = "2c:f0:5d:54:49:17";
+        }
+      ];
+      extraConfig = ''
+        option domain-name-servers 8.8.8.8;
+        option subnet-mask 255.255.255.0;
+
+        subnet 10.42.0.0 netmask 255.255.255.0 {
+          option broadcast-address 10.42.255.255;
+          option routers 10.42.0.1;
+          interface ethlan0;
+          range 10.42.0.200 10.42.255.254;
+        }
+      '';
+    };
   };
   outCfg = lib.nixosSystem {
     inherit system;
